a71fec43a9b7deca50e717eb5c79f234866fdf35
PedroHe18 <pedrohenriquesr68@gmail.com>
2026-09-17 18:04:28 -0300

    Reforma 17/09/2026 Geral

--- Produtos/script.js
+++ Produtos/script.js
@@ -1,3 +1,4 @@
+//ampliar e diminuir logo do cabeçalho
 window.addEventListener('scroll', function() {
   const header = document.getElementById('meu-header');
   
@@ -6,4 +7,17 @@ window.addEventListener('scroll', function() {
   } else {
     header.classList.remove('rolado');
   }
-});
+})
+
+//Expandir menu da sacola
+const btnCarrinho = document.getElementById('btn-carrinho')
+const carrinhoSidebar = document.getElementById('carrinho-sidebar')
+const fecharCarrinho = document.getElementById ('fechar-carrinho')
+
+btnCarrinho.addEventListener('click', () => {
+  carrinhoSidebar.classList.add('aberto')
+})
+
+fecharCarrinho.addEventListener('click', () => {
+  carrinhoSidebar.classList.remove('aberto')
+})
